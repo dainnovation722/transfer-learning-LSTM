6dfa93b4a9825c21f524dd759eb55961f4862888
dainnovation722 <722.dai.innovation@gmail.com>
2019-11-12 23:33:38 +0900

    pickleファイルが無いsourceデータセットに対応

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ import pickle
 from time import time
 
 def read_data_from_dataset(dname:str) -> np.array:
-	'''データセットから学習データとテストデータを読み込む'''
-    data_list = []
+    '''データセットから学習データとテストデータを読み込む'''
+    data_list=[]
     for fname in ['X_train', 'y_train', 'X_test', 'y_test']:
         with open(f'dataset/{dname}/{fname}.pkl', 'rb') as f:
             data = pickle.load(f)
@@ -25,8 +25,8 @@ def read_data_from_dataset(dname:str) -> np.array:
     return data_list[0], data_list[1], data_list[2], data_list[3]
 
 def generator(X:np.array, y:np.array) -> np.array:
-	'''時系列バッチを取得する'''
-	time_steps = 1000 if X_test.shape[0]//2>1000 else X_test.shape[0]//2 #ここのハイパラは調整する価値あり!
+    '''時系列バッチを取得する'''
+    time_steps = 1000 if X_test.shape[0]//2 > 1000 else X_test.shape[0]//2 # ここのハイパラは調整する価値あり!
     n_batches = X.shape[0] - time_steps - 1
     X_time = np.zeros((n_batches, time_steps, X.shape[1]))
     y_time = np.zeros((n_batches, 1))    
@@ -65,7 +65,7 @@ def build_model(input_shape:tuple, pre_model=None) -> list:
 def train(pre_model=None):
 	'''学習実行'''
 	mini_batch_size = X_train_time.shape[0]//10
-
+	
 	start_time = time()
 
 	input_shape = (X_train_time.shape[1], X_train_time.shape[2]) # x_train.shape[2] is num of variable
@@ -98,8 +98,10 @@ if __name__ == '__main__':
 	verbose = 1
 	
 	if sys.argv[1] == 'pre-train':
-
+		
 		for source in os.listdir('dataset'):
+			# sourceデータセットにpickleファイルがない場合は次のsourceデータセットへ
+			if not os.path.exists(f'dataset/{source}/X_train.pkl'): continue
 			# データセットの読み込み
 			X_train, y_train, X_test, y_test = read_data_from_dataset(source)
 			X_train_time, y_train_time = generator(X_train, y_train)
@@ -109,19 +111,23 @@ if __name__ == '__main__':
 			if not os.path.exists(write_output_dir): os.makedirs(write_output_dir)
 			file_path = write_output_dir + 'best_model.hdf5'
 			print('Learning from '+source)
+			# 学習打ち切り
+			early_stopping = EarlyStopping(patience=5)
 			# 学習スケジューラー
 			reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.5, patience=50,
 					min_lr=0.0001)
 			# モデルチェックポイント
 			model_checkpoint = keras.callbacks.ModelCheckpoint(filepath=file_path, monitor='loss',
 				save_best_only=True)
-			callbacks=[reduce_lr,model_checkpoint]
+			callbacks=[early_stopping, reduce_lr,model_checkpoint]
 
 			train()
 
 	if sys.argv[1] == 'transfer-learning':
 		for source in os.listdir('dataset'): 
-			for target in ['sru','debutanizer']:
+			# sourceデータセットにpickleファイルがない場合は次のsourceデータセットへ
+			if not os.path.exists(f'dataset/{source}/X_train.pkl'): continue
+			for target in ['sru','debutanizer']: 
 				if target == source: continue
 				# データセットの読み込み
 				X_train, y_train, X_test, y_test = read_data_from_dataset(source)
@@ -134,12 +140,14 @@ if __name__ == '__main__':
 				print('Tranfering from '+source+' to '+target)
 				# 事前学習済みモデルの読み込み
 				pre_model = load_model(f'pre-train/{source}/best_model.hdf5')
+				# 学習打ち切り
+				early_stopping = EarlyStopping(patience=5)
 				# 学習スケジューラー
 				reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.5, patience=50,
 					min_lr=0.0001)
 				# モデルチェックポイント
 				model_checkpoint = keras.callbacks.ModelCheckpoint(filepath=file_path, monitor='loss',
 					save_best_only=True)
-				callbacks=[reduce_lr,model_checkpoint]
+				callbacks=[early_stopping, reduce_lr,model_checkpoint]
 
 				train(pre_model)
