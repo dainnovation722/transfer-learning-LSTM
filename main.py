7f90f46da42a11376b6fb1dced0d6f6698aee739
dainnovation722 <722.dai.innovation@gmail.com>
2019-11-13 13:01:06 +0000

    学習結果の図を保存

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from keras.models import Model, load_model
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
 import keras
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 plt.rcParams["font.size"] = 18
 
@@ -26,7 +28,10 @@ def read_data_from_dataset(dname:str) -> np.array:
 
 def generator(X:np.array, y:np.array) -> np.array:
     '''時系列バッチを取得する'''
-    time_steps = 1000 if X_test.shape[0]//2 > 1000 else X_test.shape[0]//2 # ここのハイパラは調整する価値あり!
+    if X_train.shape[0] > X_test.shape[0]:
+	    time_steps = 1000 if X_test.shape[0]//2 > 1000 else X_test.shape[0]//2 # ここのハイパラは調整する価値あり!
+    else:
+        time_steps = 1000 if X_train.shape[0]//2 > 1000 else X_train.shape[0]//2 # ここのハイパラは調整する価値あり!
     n_batches = X.shape[0] - time_steps - 1
     X_time = np.zeros((n_batches, time_steps, X.shape[1]))
     y_time = np.zeros((n_batches, 1))    
@@ -62,9 +67,27 @@ def build_model(input_shape:tuple, pre_model=None) -> list:
 
 	return model
 
+def save_fig(hist, y_pred_time):
+	# 学習曲線の保存
+	plt.figure(figsize=(18,5))
+	plt.plot(hist.history['loss'])
+	plt.plot(hist.history['val_loss'])
+	plt.title('model loss')
+	plt.ylabel('loss')
+	plt.xlabel('epoch')
+	plt.legend(['train', 'test'], loc='upper right')
+	plt.savefig(write_output_dir + 'learning_curve.png')
+	# テストデータの予測結果の保存
+	plt.figure(figsize=(18,5))
+	plt.plot([ i for i in range(1, 1+len(y_pred_time))], y_pred_time, 'r',label="predicted")
+	plt.plot([ i for i in range(1, 1+len(y_test_time))], y_test_time, 'b',label="measured", lw=1, alpha=0.3)
+	plt.ylim(0,1)
+	plt.legend(loc="best")
+	plt.savefig(write_output_dir + 'prediction.png')
+	
 def train(pre_model=None):
 	'''学習実行'''
-	mini_batch_size = X_train_time.shape[0]//10
+	mini_batch_size = X_train_time.shape[0]//20
 	
 	start_time = time()
 
@@ -74,7 +97,7 @@ def train(pre_model=None):
 	if verbose == True: 
 		model.summary()
 
-	
+
 	hist = model.fit(X_train_time, y_train_time, batch_size=mini_batch_size, epochs=nb_epochs,
 		verbose=verbose, validation_data=(X_test_time, y_test_time), callbacks=callbacks)
 
@@ -82,19 +105,22 @@ def train(pre_model=None):
 	model = load_model(file_path)
 
 	y_pred_time = model.predict(X_test_time)
+	
+	save_fig(hist,y_pred_time) #予測モデル図追加
+	
 	accuracy = mse(y_test_time,y_pred_time)
 	
-	duration = time.time()-start_time
+	duration = time()-start_time
 
-	with open(write_output_dir+'log.txt','wb') as f:
-		f.write('duration : {:.3f}'.format(duration))
+	with open(write_output_dir+'log.txt','w') as f:
+		f.write('duration : {:.3f}\n'.format(duration))
 		f.write('accuracy : {:.3f}'.format(accuracy))
 
 	keras.backend.clear_session()
 
 if __name__ == '__main__':
 
-	nb_epochs = 30
+	nb_epochs = 50
 	verbose = 1
 	
 	if sys.argv[1] == 'pre-train':
@@ -107,10 +133,11 @@ if __name__ == '__main__':
 			X_train_time, y_train_time = generator(X_train, y_train)
 			X_test_time, y_test_time = generator(X_test, y_test)
 			# 保存先フォルダー作成
-			write_output_dir = 'pre-train/{source}/'
+			write_output_dir = f'pre-train/{source}/'
 			if not os.path.exists(write_output_dir): os.makedirs(write_output_dir)
 			file_path = write_output_dir + 'best_model.hdf5'
 			print('Learning from '+source)
+			print(f'data shape : {X_train_time.shape}')
 			# 学習打ち切り
 			early_stopping = EarlyStopping(patience=5)
 			# 学習スケジューラー
@@ -124,11 +151,12 @@ if __name__ == '__main__':
 			train()
 
 	if sys.argv[1] == 'transfer-learning':
-		for source in os.listdir('dataset'): 
-			# sourceデータセットにpickleファイルがない場合は次のsourceデータセットへ
-			if not os.path.exists(f'dataset/{source}/X_train.pkl'): continue
-			for target in ['sru','debutanizer']: 
-				if target == source: continue
+		targets = ['sru','debutanizer']
+		for target in targets: 
+			for source in os.listdir('dataset'): 
+				# sourceデータセットにpickleファイルがない場合は次のsourceデータセットへ
+				if not os.path.exists(f'dataset/{source}/X_train.pkl'): continue		
+				if source in targets: continue
 				# データセットの読み込み
 				X_train, y_train, X_test, y_test = read_data_from_dataset(source)
 				X_train_time, y_train_time = generator(X_train, y_train)
