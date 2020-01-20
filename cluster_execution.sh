b4e2fff362db4e08e56cc9b8d7b7f8faedd362b4
dainnovation722 <722.dai.innovation@gmail.com>
2020-01-20 12:50:25 +0000

    ✨ add shell parameters

--- cluster_execution.sh
+++ cluster_execution.sh
@@ -7,4 +7,13 @@ cd $PBS_O_WORKDIR
 . ~/anaconda3/etc/profile.d/conda.sh
 conda activate gpu
 
-python main.py pre-train && python main.py transfer-learning
+condition="test"
+n_iteration=3
+
+
+for n_iter in `seq 1 $n_iteration`
+do
+    python main.py -m pre-train -n "${condition}_${n_iter}"
+    python main.py -m transfer-learning -n "${condition}_${n_iter}"
+    python main.py -m without-transfer-learning -n "${condition}_${n_iter}"   
+done
