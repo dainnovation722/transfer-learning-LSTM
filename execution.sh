ac048400549ce98d549ce325486b05545b1a13e3
dainnovation722 <722.dai.innovation@gmail.com>
2020-01-20 12:30:31 +0000

    🔧 add shell parameter for experiment iteration and condition name

--- execution.sh
+++ execution.sh
@@ -1,1 +1,16 @@
-python main.py pre-train && python main.py transfer-learning
+#!/bin/bash
+
+# python main.py pre-train && python main.py transfer-learning && python main.py without-transfer-learning
+
+#事例データ
+
+condition="test"
+n_iteration=3
+
+
+for n_iter in `seq 1 $n_iteration`
+do
+    python main.py -m pre-train -n "${condition}_${n_iter}"
+    python main.py -m transfer-learning -n "${condition}_${n_iter}"
+    python main.py -m without-transfer-learning -n "${condition}_${n_iter}"   
+done
