53bd54499c8435b02561b2797c9c4aa9e87da40c
dainnovation722 <722.dai.innovation@gmail.com>
2020-01-21 11:45:34 +0000

    🐎 add seed for initial weights

--- utils/models.py
+++ utils/models.py
@@ -3,7 +3,7 @@ from keras.layers import CuDNNLSTM as LSTM
 from keras.layers.wrappers import TimeDistributed
 from keras.models import Model
 from keras.optimizers import Adam
-
+from keras import initializers
 
 class regressor():
 
@@ -15,18 +15,23 @@ class regressor():
 		'''学習モデルを構築'''
 		input_layer = Input(input_shape)
 
-		dense = TimeDistributed(Dense(50))(input_layer)
+		dense = TimeDistributed(Dense(50, kernel_initializer=initializers.glorot_uniform(seed=0), 
+			bias_initializer=initializers.Zeros()))(input_layer)
 
-		lstm1 = LSTM(25, return_sequences=True)(dense)
+		lstm1 = LSTM(25, return_sequences=True, kernel_initializer=initializers.glorot_uniform(seed=0),
+			recurrent_initializer=initializers.Orthogonal(seed=0), bias_initializer=initializers.Zeros())(dense)
 		lstm1 = BatchNormalization()(lstm1)
 
-		lstm2 = LSTM(50, return_sequences=True)(lstm1)
+		lstm2 = LSTM(50, return_sequences=True, kernel_initializer=initializers.glorot_uniform(seed=0),
+			recurrent_initializer=initializers.Orthogonal(seed=0), bias_initializer=initializers.Zeros())(lstm1)
 		lstm2 = BatchNormalization()(lstm2)
 
-		lstm3 = LSTM(25, return_sequences=False)(lstm2)
+		lstm3 = LSTM(25, return_sequences=False, kernel_initializer=initializers.glorot_uniform(seed=0),
+			recurrent_initializer=initializers.Orthogonal(seed=0), bias_initializer=initializers.Zeros())(lstm2)
 		lstm3 = BatchNormalization()(lstm3)
 
-		output_layer = Dense(1, activation='sigmoid')(lstm3)
+		output_layer = Dense(1, activation='sigmoid', kernel_initializer=initializers.glorot_uniform(seed=0), 
+			bias_initializer=initializers.Zeros())(lstm3)
 
 		model = Model(inputs=input_layer, outputs=output_layer)
 
@@ -34,7 +39,7 @@ class regressor():
 			for i in range(2,len(model.layers)): 
 				model.layers[i].set_weights(pre_model.layers[i].get_weights())
 
-		model.compile(loss='mse', optimizer = Adam(), metrics=['accuracy'])
+		model.compile(optimizer = Adam(), loss='mse', metrics=['accuracy'])
 		if verbose: print(model.summary())
 		return model
 		
