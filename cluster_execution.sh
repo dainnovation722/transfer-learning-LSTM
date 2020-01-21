6cb5ae219cecaea71ee914bf5cf095807d67b7d7
dainnovation722 <722.dai.innovation@gmail.com>
2020-01-21 11:42:12 +0000

    🐛 add AND operator

--- cluster_execution.sh
+++ cluster_execution.sh
@@ -13,7 +13,7 @@ n_iteration=3
 
 for n_iter in `seq 1 $n_iteration`
 do
-    python main.py -m pre-train -n "${condition}_${n_iter}"
-    python main.py -m transfer-learning -n "${condition}_${n_iter}"
+    python main.py -m pre-train -n "${condition}_${n_iter}" && \
+    python main.py -m transfer-learning -n "${condition}_${n_iter}" && \
     python main.py -m without-transfer-learning -n "${condition}_${n_iter}"   
 done
