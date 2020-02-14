5a86045f6f5e5a0f3829d2d4d66b1fdaf6391a62
dainnovation722 <722.dai.innovation@gmail.com>
2020-02-14 08:45:13 +0000

    🐎 output png and csv for results

--- execution.sh
+++ execution.sh
@@ -4,13 +4,14 @@
 
 #事例データ
 
-condition="test"
-n_iteration=3
+condition="transfer_in_only_lstm"
+n_iteration=1
 
 
 for n_iter in `seq 1 $n_iteration`
 do
     python main.py -m pre-train -n "${condition}_${n_iter}" && \
     python main.py -m transfer-learning -n "${condition}_${n_iter}" && \
-    python main.py -m without-transfer-learning -n "${condition}_${n_iter}"   
+    python main.py -m without-transfer-learning -n "${condition}_${n_iter}" && \
+    python main.py -m output-results -n "${condition}_${n_iter}"
 done
