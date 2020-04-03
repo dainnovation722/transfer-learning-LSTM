ebea6376291ddc0eadd5b1c4118f581166725b04
dainnovation722 <722.dai.innovation@gmail.com>
2020-04-03 14:24:01 +0900

    🎨 dealing with argparser arguments

--- execution.sh
+++ execution.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-# python main.py pre-train && python main.py transfer-learning && python main.py without-transfer-learning
+python main.py \
+    --train-mode pre-train \
+    --out-dir test \
+    --gpu \
 
-#事例データ
-
-condition="transfer_in_only_lstm"
-n_iteration=1
-
-
-for n_iter in `seq 1 $n_iteration`
-do
-    python main.py -m pre-train -n "${condition}_${n_iter}" && \
-    python main.py -m transfer-learning -n "${condition}_${n_iter}" && \
-    python main.py -m without-transfer-learning -n "${condition}_${n_iter}" && \
-    python main.py -m output-results -n "${condition}_${n_iter}"
-done
+    
