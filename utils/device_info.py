e3f93148da2c9ee9d142419db81a49c79d61d108
dainnovation722 <722.dai.innovation@gmail.com>
2020-04-03 14:21:17 +0900

    📚 remove function adding

--- utils/device_info.py
+++ utils/device_info.py
@@ -1,5 +1,9 @@
 from tensorflow.python.client import device_lib
+import os
+
 with open('utils/device_info.txt','w') as f:
     f.write('\n')
     f.write(str(device_lib.list_local_devices()))
     f.write('\n')
+
+os.remove('utils/device_info.txt')
