fd3221f605ca2bbcc21b1229cc4d253bb35f90c0
dainnovation722 <722.dai.innovation@gmail.com>
2020-04-06 15:09:29 +0900

    🎨 dealing with new argpaser

--- cluster_execution.sh
+++ cluster_execution.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
-#PBS -l select=1:ngpus=1
-#PBS -q gpuq_cuda10
-#PBS -N relu-TL
+#PBS -l select=1:ngpus=1:vnode=BDGPU11
+#PBS -q gpuq_V100_32
+#PBS -N without_tl
+#PBS -o job_archive
+#PBS -e job_archive
 
 cd $PBS_O_WORKDIR
 . ~/anaconda3/etc/profile.d/conda.sh
 conda activate gpu
 
-condition="test"
-n_iteration=3
-
-
-for n_iter in `seq 1 $n_iteration`
-do
-    python main.py -m pre-train -n "${condition}_${n_iter}" && \
-    python main.py -m transfer-learning -n "${condition}_${n_iter}" && \
-    python main.py -m without-transfer-learning -n "${condition}_${n_iter}"   
-done
+output="test1"
+python main.py -m pre-train -o "${output}" --gpu && \
+python main.py -m transfer-learning -o "${output}" --gpu && \
+python main.py -m without-transfer-learning -o "${output}" --gpu && \
+python main.py -m score -o "${output}" --gpu  
