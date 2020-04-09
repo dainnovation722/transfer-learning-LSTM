a6dbd6fdc60c8863d2b787fc8292a4b33dd1d616
dainnovation722 <722.dai.innovation@gmail.com>
2020-04-09 14:44:08 +0900

    🔧 add argument for epoch

--- cluster_execution.sh
+++ cluster_execution.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-#PBS -l select=1:ngpus=1:vnode=BDGPU11
+#PBS -l select=1:ngpus=1:vnode=BDGPU10
 #PBS -q gpuq_V100_32
-#PBS -N without_tl
+#PBS -N long_train
 #PBS -o job_archive
 #PBS -e job_archive
 
 cd $PBS_O_WORKDIR
 . ~/anaconda3/etc/profile.d/conda.sh
-conda activate gpu
+conda activate keras
 
-output="test1"
-python main.py -m pre-train -o "${output}" --gpu && \
-python main.py -m transfer-learning -o "${output}" --gpu && \
-python main.py -m without-transfer-learning -o "${output}" --gpu && \
-python main.py -m score -o "${output}" --gpu  
+output="100_epochs"
+python main.py -m pre-train -o "${output}" --gpu --nb-epochs 100 && \
+python main.py -m transfer-learning -o "${output}" --gpu --nb-epochs 100 && \
+python main.py -m without-transfer-learning -o "${output}" --gpu --nb-epochs 100 && \
+python main.py -m score -o "${output}" 
--- train_all.sh
+++ train_all.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-python main.py -m pre-train --gpu && \
-python main.py -m transfer-learning --gpu && \
-python main.py -m without-transfer-learning --gpu && \
-python main.py -m score --gpu  
+output="100_epochs"
+epochs="100"
+python main.py -m pre-train -o ${output} --gpu --nb-epochs $epochs && \
+python main.py -m transfer-learning -o ${output} --gpu --nb-epochs $epochs && \
+python main.py -m without-transfer-learning -o ${output} --gpu --nb-epochs $epochs && \
+python main.py -m score -o ${output}
